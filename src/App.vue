12cf7c582f1f261b568533585d6ac295b092b72c
ratamanov <r399055750@gmail.com>
2021-11-11 17:19:24 +0500

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="list-data">
+        <div class="search">
+          <input type="text" class="input" v-model="inputSearch" placeholder="Поиск">
+          <div v-show="inputSearch" @click="inputSearch = ''" class="btn-clear"></div>
+        </div>
+        <div class="row-item" v-for="item in listUsersPhones" :key="item.id">
+          <toggle-item :item="item">
+          </toggle-item>
+        </div>
+      </div><!--end list-data -->
+      {{pages}}
+      <ol class="pagination">
+        <li v-for="(pageNumber, index) in pages" :key="index"><button type="button" class="btn btn-sm btn-outline-secondary"  @click="getUsers(pageNumber)"> {{pageNumber}} </button></li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import toggleItem from './components/toggleItem'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    toggleItem
+  },
+  data() {
+    return {
+      listUsers: [],
+      inputSearch: '',
+      isVisible: false,
+      page: 1,
+      perPage: 6,
+      totalPage: null,
+      pages: [],
+    }
+  },
+  computed:{
+    // eslint-disable-next-line vue/return-in-computed-property
+    listUsersPhones(){
+      const inpSearch = this.inputSearch.toLowerCase();
+      if (inpSearch === '' || !inpSearch) {
+       return this.paginate(this.listUsers.map(name => ({...name, phone: this.phoneGenerator()})));
+      }else {
+        return this.listUsers.filter((inp) =>
+          inp.first_name.toLowerCase().includes(inpSearch)
+        )
+      }
+    },
+  },
+  created() {
+    this.getUsers(this.page);
+  },
+  watch: {
+    listUsers () {
+      this.setPages(this.page);
+    }
+  },
+  mounted() {
+    window.setInterval(() => {
+      this.getUsers()
+    }, 60000)
+    },
+  methods: {
+    phoneGenerator(){
+      let phoneNums = Math.random().toString().slice(2,11);
+      return phoneNums.slice(0,3)+"-"+phoneNums.slice(3,6)+"-"+phoneNums.slice(6);
+    },
+    async getUsers(page){
+      try {
+      console.log('запрос к api каждую минуту')
+      const {data: { data: listUsers }, total: totalPage } = await axios.get('https://reqres.in/api/users?page='+page+'&per_page='+this.perPage);
+        this.listUsers = listUsers;
+        this.totalPage = totalPage;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    setPages () {
+      this.pages = [];
+      let numberOfPages = Math.ceil(this.totalPage / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
+    },
+    paginate (listUsersPhones) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = (page * perPage) - perPage;
+      let to = (page * perPage);
+      return  listUsersPhones.slice(from, to);
+    },
+
   }
 }
 </script>
@@ -21,8 +105,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.row-item {
+  border: 1px solid #888;
+  padding: 5px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+}
+.title {
+  font-weight: bold;
+  padding-bottom: 5px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/toggleItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="toggle-row">
+    <div class="title" @click="isVisible = !isVisible">{{ item.first_name }}</div>
+    <div class="toggle" v-show="isVisible">
+      <div class="row">
+        <div class="col-1">{{ item.id }}</div>
+        <div class="col-1"><img :src="item.avatar" width="60" alt=""></div>
+        <div class="col-3">{{ item.email }}</div>
+        <div class="col-3">{{ item.first_name }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'toggleItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => []
+    }
+  },
+  data(){
+    return {
+      isVisible: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
